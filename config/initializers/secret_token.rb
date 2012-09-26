01076d50f5fc9d2ef7038462c1526682d130a0c3
Renae Bair <renaebair@gmail.com>
2012-09-26 11:36:12 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twintweets::Application.config.secret_token = '9969d24e5f2a528162d8c111a6cb41aac7aaf159ab538eedc8f4852ea82670cf854f78c5a6b9f685bceb3fde342b03226111326bdb5e1207760d97326f9e3659'
