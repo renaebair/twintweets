5833e3c79f44df492baf0104cc1fd8320015a5af
Renae Bair <renaebair@gmail.com>
2012-09-26 13:48:06 -0400

    hook up interface to backend

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,6 +4,12 @@ class CitiesController < ApplicationController
   end
   
   def create
+    @city_1_name = params[:city_1]
+    @city_2_name = params[:city_2]
+    @city_1_tweets = TweetHarvester.new(params[:city_1]).harvest
+    @city_2_tweets = TweetHarvester.new(params[:city_2]).harvest
+    @city_1_score = TweetAnalyzer.new(@city_1_tweets).analyze
+    @city_2_score = TweetAnalyzer.new(@city_2_tweets).analyze
     render :show
   end
 end
